19a330408a8bee11550c86984be4e9bb6f99ddb7
Darvetra <darvetra13@gmail.com>
2022-05-10 14:43:29 +0300

    рефакторит компонент not-found

--- project/src/components/not-found/not-found.tsx
+++ project/src/components/not-found/not-found.tsx
@@ -1,9 +1,11 @@
+import {Link} from 'react-router-dom';
+
 function NotFound(): JSX.Element {
   return (
     <section style={{padding: '20px'}}>
       <h1>404.</h1>
       <p>Page not found</p>
-      <a href="/" style={{fontWeight: 'bold', textDecoration: 'underline'}}>Вернуться на главную страницу</a>
+      <Link to="/" style={{fontWeight: 'bold', textDecoration: 'underline'}}>Вернуться на главную страницу</Link>
     </section>
   );
 }
