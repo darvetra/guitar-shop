f25b286920afbf1a5d9ef8265257be4e701c52e0
Darvetra <darvetra13@gmail.com>
2022-05-18 19:50:03 +0300

    подключает Redux

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import App from './components/app/app';
+import {reducer} from './store/reducer';
 
 import {guitarList} from './mocks/guitars';
 import {reviewList} from './mocks/reviews';
@@ -8,12 +12,16 @@ import {reviewList} from './mocks/reviews';
 // Доделать. Должен подтягиваться товар на который произведен клик, а не первый из массива
 const product = guitarList[0];
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      products={guitarList}
-      product={product}
-      reviews={reviewList}
-    />
+    <Provider store = {store}>
+      <App
+        products={guitarList}
+        product={product}
+        reviews={reviewList}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
